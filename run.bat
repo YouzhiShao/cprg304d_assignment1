973847395dbb6fdaca1c15774204d5fb4d877143
YouzhiShao <youzhi.shao@edu.sait.ca>
2026-02-27 23:42:34 -0700

    add run.bat

---
+++ run.bat
@@ -0,0 +1,2 @@
+java -jar Sort.jar -ta -sQ -fres\shapes3.txt
+pause
--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -33,7 +33,10 @@ public class AppDriver
 		// refer to demo02 KittySort.java on how to use a custom sorting
 		// algorithm on a list of comparables to sort using either the
 		// natural order (comparable) or other orders (comparators)
-
+		if(args.length == 0){
+			System.out.println("parameter error");
+			return;
+		}
 		new SortManager(args);
 	}
 
